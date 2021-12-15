5eb0a0078d83d348586c15485d171a1067f2f379
ngortalova <tesla@list.ru>
2021-12-15 09:27:50 +0300

    изменила продактс страницу. теперь выводит главную картику в соответствии с категорией, а не только Похожее

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from datetime import datetime
 from .models import ProductCategory, Product
+import random
 
 menu_links = [
     {'view_name': 'index', 'name': 'домой'},
@@ -48,8 +49,10 @@ def contact(request):
 def products(request, pk=None):
     products_new = Product.objects.filter(category_id=pk)
     product_category = ProductCategory.objects.all()
+    products = random.sample(list(Product.objects.all()), 3)
     return render(request, 'mainapp/products.html', context={'menu_links': menu_links,
                                                              'products_new': products_new,
                                                              'container_block_class': "hero-white",
                                                              "product_category": product_category,
+                                                             "products": products,
                                                              })
