bbaa8bb5248100a7fb7a00a2a600f9c2ccdb7d74
ngortalova <tesla@list.ru>
2022-01-04 13:22:25 +0300

    template tag mainapp products

--- cartapp/models.py
+++ cartapp/models.py
@@ -20,7 +20,7 @@ class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="cart",
                              verbose_name="Пользователь")
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Продукт")
-    quantity = models.PositiveIntegerField(verbose_name="Колличество", default=0)
+    quantity = models.PositiveIntegerField(verbose_name="Количество", default=0)
     add_datetime = models.DateTimeField(verbose_name="Время", auto_now_add=True)
 
     objects = CartManager()
